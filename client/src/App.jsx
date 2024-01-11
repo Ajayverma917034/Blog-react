19dde50df2c9d51901acdc168995e8f65fd7ea56
Ajay Verma <ajayverma917034@gmail.com>
2024-01-11 13:21:29 +0530

    update blog-detial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
         <UserContext.Provider value={{userAuth, setUserAuth}}>
             <Routes>
                 <Route path="/editor" element={<Editor/>}/>
+                <Route path="/editor/:blog_id" element={<Editor/>}/>
                 <Route path="/" element={<Navbar/>}>
                     <Route index element={<Home/>}/>
                     <Route path="signin" element={<UserAuthForm type="sign-in"/>}/>
